2462599f2231949a66f8225fe663857b885e33e9
Ilia Bozhinov <ammen99@gmail.com>
2019-10-08 10:18:13 +0300

    render-manager: add constant delay to frame rendering

--- src/output/render-manager.cpp
+++ src/output/render-manager.cpp
@@ -329,6 +329,9 @@ class wf::render_manager::impl
 {
   public:
     wf::wl_listener_wrapper on_frame;
+    wf::wl_listener_wrapper on_present;
+    wf::wl_timer repaint_timer;
+    uint64_t refresh_nsec;
 
     output_t *output;
     std::unique_ptr<output_damage_t> output_damage;
@@ -349,7 +352,29 @@ class wf::render_manager::impl
         effects = std::make_unique<effect_hook_manager_t> ();
         postprocessing = std::make_unique<postprocessing_manager_t>(o);
 
-        on_frame.set_callback([&] (void*) { paint(); });
+        on_present.set_callback([&] (void *data) {
+            auto ev = static_cast<wlr_output_event_present*> (data);
+            this->refresh_nsec = ev->refresh;
+        });
+        on_present.connect(&output->handle->events.present);
+
+        on_frame.set_callback([&] (void*) {
+            /*
+             * Leave a bit of time for clients to render, see
+             * https://github.com/swaywm/sway/pull/4588
+             */
+            const uint64_t max_render_time = 7;
+            uint64_t total = this->refresh_nsec / 1000000 - max_render_time;
+            if (total <= 0 || max_render_time <= 0 || this->renderer)
+                total = 0;
+
+            output->handle->block_idle_frame = true;
+            log_info("delay frame by %lu", total);
+            repaint_timer.set_timeout(total, [=] () {
+                output->handle->block_idle_frame = false;
+                paint();
+            });
+        });
         on_frame.connect(&output_damage->damage_manager->events.frame);
 
         init_default_streams();
